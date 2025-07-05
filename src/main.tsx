f14a2b200bf42a80bc556999f8f23464b0ec6397
sfuentes <dev@sfuentes.cl>
2025-07-05 05:13:37 -0300

    Moved Tailwindcss import to main.css and added Inter font
    Because when tailwindcss is imported into index.html or in <style> tags, the Tailwindcss extensions don't work correctly

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import Router from "./components/Router.tsx";
+import Router from "./lib/Router.tsx";
 import LanguageProvider from "./components/LanguageContext.tsx";
+import "./main.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
